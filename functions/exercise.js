8fa7cd843273eff75c85e7c9f02757aa5b923ceb
EvelynNamubiru <lynivy12@gmail.com>
2022-04-11 13:03:24 +0300

    add picking out even numbers from an array

--- functions/exercise.js
+++ functions/exercise.js
@@ -21,5 +21,15 @@ function recursive(num){
 }
 }
 
+function evenNumber(arr){
+    var even =[];
+    for(var i =0;i<arr.length;i++){
+        if(arr[i]%2 ==0){
+            even.push(arr[i]);
+        }
+    }
+      return even;
+}
+
 
 
